5ae2bf4a4e4d75d94bb071482a13a3c56cf7dbb3
leegeunhyeok <lghlove0509@naver.com>
2019-04-12 18:32:19 +0900

    FIXED: Install issue fixed

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-$(git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && 
-\ vim --not-a-term +PluginInstall +qall)
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 if [ $? -eq 0 ]; then
   echo `cp ./.vimrc ~/.vimrc`
   echo "VimSCode was installed!"
-  exit 0 
+  exit 0
 fi
 
 echo "ERROR: Please check the installation manual"
+exit -1
