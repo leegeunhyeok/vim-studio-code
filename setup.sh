47900024345e2ea557fa72e56ab623d0edeba3fa
leegeunhyeok <lghlove0509@naver.com>
2019-04-12 16:09:32 +0900

    ADD: setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+$(git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && 
+\ vim --not-a-term +PluginInstall +qall)
+
+if [ $? -eq 0 ]; then
+  echo `cp ./.vimrc ~/.vimrc`
+  echo "VimSCode was installed!"
+  exit 0 
+fi
+
+echo "ERROR: Please check the installation manual"
